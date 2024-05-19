07c0b318e3ff27240c1565491f45e6fe2b1ff6c7
Efe Özgen <efe.ozgennn@gmail.com>
2024-05-19 12:27:21 +0300

    brnv alu32 last

--- alu32.v
+++ alu32.v
@@ -5,32 +5,35 @@ input [2:0] gin;//ALU control line
 reg [31:0] sum;
 reg [31:0] less;
 output zout;
-output overflow;
+output reg overflow;
 reg zout;
 always @(a or b or gin)
-overflow = 0;
 begin
     case(gin)
     3'b010: begin sum=a+b;         //ALU control line=010, ADD
     //overflow detection
-    if(a[31]==b[31] && sum[31]!=a[31]) begin
-        overflow=1;
+    overflow = ((a[31] & b[31] & ~sum[31]) | (~a[31] & ~b[31] & sum[31]));
             end
-        end
 
     3'b110: begin sum=a+1+(~b);    //ALU control line=110, SUB
     //overflow detection
-    if(a[31]!=b[31] && sum[31]!=a[31]) begin
-        overflow=1;
+    overflow = ((a[31] & ~b[31] & ~sum[31]) | (~a[31] & b[31] & sum[31]));
             end
-        end
 
-    3'b111: begin less=a+1+(~b);    //ALU control line=111, set on less than
+    3'b111: begin 
+		overflow=0;
+		less=a+1+(~b);    //ALU control line=111, set on less than
             if (less[31]) sum=1;
             else sum=0;
           end
-    3'b000: sum=a & b;    //ALU control line=000, AND
-    3'b001: sum=a|b;        //ALU control line=001, OR
+    3'b000: begin
+		overflow=0;
+		sum=a & b;    //ALU control line=000, AND
+			end
+    3'b001: begin
+		overflow=0;
+		sum=a|b;        //ALU control line=001, OR
+			end
     default: sum=31'bx;
     endcase
 zout=~(|sum);
