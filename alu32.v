a5f9b739f004c94b48683c26c3315049dd9a645e
Mustafa Tolga Akbaba <musmeh10@gmail.com>
2024-05-22 17:32:05 +0300

    alu32 fixed!

--- alu32.v
+++ alu32.v
@@ -1,40 +1,53 @@
-module alu32(sum, a, b, zout, N, V);
-  output [31:0] sum;
-  input [31:0] a, b;
-  input [2:0] gin; // ALU control line
-  reg [31:0] sum;
-  reg [31:0] less;
-  output zout;
-  reg zout;
-  output reg N;
-  output reg V;
+module alu32(sum, a, b, gin,0, zout, N, V);
+output [31:0] sum;
+input [31:0] a, b;
+input [2:0] gin; // ALU control line
+reg [31:0] sum;
+reg [31:0] less;
+output zout;
+reg zout;
+output reg N;
+output reg V;
 
   always @(a or b or gin)
   begin
     case (gin)
-      3'b010: sum = a + b; // ALU control line=010, ADD
-        V = (a[31] == b[31]) && (sum[31] != a[31]); // Check for overflow in addition
-      3'b110: sum = a + 1 + (~b); // ALU control line=110, SUB
-        V = (a[31] != b[31]) && (sum[31] == a[31]); // Check for overflow in subtraction
-      3'b111: begin // ALU control line=111, set on less than
-        less = a + 1 + (~b);
+      3'b010: 
+	begin 
+	sum <= a + b; // ADD
+        V = (a[31] == b[31]) && (sum[31] != a[31]); // Check for overflow
+	end
+      3'b110: 
+	begin
+	sum <= a + 1'b1 + (~b); // SUB
+        V <= (a[31] != b[31]) && (sum[31] == a[31]); // Check for overflow
+        end
+      3'b111: 
+	begin // Set on less than
+        less <= a + 1'b1 + (~b);
         if (less[31]) begin
-          sum = 1;
-          N = 1; // Set negative flag if result is negative
+          sum <= 32'b1;
+          N <= 1; // Set negative flag if result is negative
         end else begin
-          sum = 0;
-          N = 0;
+          sum <= 32'b0;
+          N <= 0;
         end
       end
-      3'b000: sum = a & b; // ALU control line=000, AND
-        N = sum[31]; // Negative flag based on MSB of result
-        V = 0; // No overflow for AND
-      3'b001: sum = a | b; // ALU control line=001, OR
-        N = sum[31]; // Negative flag based on MSB of result
-        V = 0; // No overflow for OR
-      default: sum = 31'bx;
+      3'b000: 
+	begin
+	sum <= a & b; // AND
+        N <= sum[31]; // Negative flag based on MSB of result
+        V <= 0;
+	end // No overflow for AND
+      3'b001: 
+	begin
+	sum <= a | b; // OR
+        N <= sum[31]; // Negative flag based on MSB of result
+        V <= 0; // No overflow for OR
+	end
+      default: sum <= 32'bx;
     endcase
-    zout = ~(|sum);
+    zout <= ~(|sum);
   end
 endmodule
 
--- control.v
+++ control.v
@@ -1,6 +1,6 @@
 module control(in,regdest,alusrc,memtoreg,regwrite,memread,memwrite,branch,aluop1,aluop2,ori,jrsal,baln);
 input [5:0] in;
-output regdest,alusrc,memtoreg,regwrite,memread,memwrite,branch,aluop1,aluop2,ori;
+output regdest,alusrc,memtoreg,regwrite,memread,memwrite,branch,aluop1,aluop2,ori,jrsal,baln;
 wire rformat,lw,sw,beq;
 assign rformat=~|in;
 assign lw=in[5]& (~in[4])&(~in[3])&(~in[2])&in[1]&in[0];
